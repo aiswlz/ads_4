d4197df7f9afd420e1ef2f4cc9a499f5f08e5b64
aiswlz <230787@astanait.edu.kz>
2024-05-29 23:52:13 +0600

    Main

--- src/Main.java
+++ src/Main.java
@@ -1,39 +1,39 @@
+
 public class Main {
 
     public static void main(String[] args) {
         WeightedGraph<String> weightedGraph = new WeightedGraph<>(true);
-        fillWithWeights(weightedGraph);
+        fillWithWeight(weightedGraph);
 
         System.out.println("Dijkstra:");
-        Search<String> djk = new DijkstraSearch<>(weightedGraph, "Almaty");
-        outputPath(djk, "kyzylorda");
+        Search<String> djk = new DijkstraSearch<>(weightedGraph, "Astana");
+        outputPath(djk, "Shymkent");
 
         System.out.println("--------------------------------");
 
         WeightedGraph<String> graph = new WeightedGraph<>(true);
-        fillWithWeights(graph);
+        fillWithWeight(graph);
 
         System.out.println("DFS:");
-        Search<String> dfs = new DijkstraSearch<>(graph, "Almaty");
-        outputPath(dfs, "Kyzylorda");
+        Search<String> dfs = new DijkstraSearch<>(graph, "Astana");
+        outputPath(dfs, "Shymkent");
 
         System.out.println("--------------------------------");
 
         System.out.println("BFS:");
-        Search<String> bfs = new BreadthFirstSearch<>(graph, "Almaty");
-        outputPath(bfs, "Kyzylorda");
+        Search<String> bfs = new BreadthFirstSearch<>(graph, "Astana");
+        outputPath(bfs, "Shymkent");
     }
 
-    public static void fillWithWeights(WeightedGraph<String> graph) {
-        graph.addEdge("Almaty", "Astana", 2.1);
-        graph.addEdge("Shymkent", "Atyrau", 7.8);
-        graph.addEdge("Atyrau", "Astana", 7.1);
-        graph.addEdge("Almaty", "Shymkent", 7.2);
-        graph.addEdge("Shymkent", "Astana", 3.9);
-        graph.addEdge("Astana", "Kostanay", 3.5);
-        graph.addEdge("Shymkent", "Kyzylorda", 5.4);
+    public static void fillWithWeight(WeightedGraph<String> graph) {
+        graph.addEdge("Astana", "Almaty", 2.1);
+        graph.addEdge("Semey", "Oskemen", 7.8);
+        graph.addEdge("Oskemen", "Almaty", 7.1);
+        graph.addEdge("Astana", "Semey", 7.2);
+        graph.addEdge("Semey", "Almaty", 3.9);
+        graph.addEdge("Almaty", "Ayakoz", 3.5);
+        graph.addEdge("Semey", "Shymkent", 5.4);
     }
-
 
     public static void outputPath(Search<String> search, String key) {
         for (String v : search.pathTo(key)) {
